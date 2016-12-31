bd07328f27181aa2741dda320e6bf3f4cc33f4ac
Evans Jahja <evans.jahja@yahoo.com>
2017-01-01 02:16:51 +0700

    Using Wirepattern

--- components/NofloBitcoin.js
+++ components/NofloBitcoin.js
@@ -13,34 +13,28 @@ exports.getComponent = () => {
   component.description = "This component receives data on a single input\
   port and sends the same data out to the output port";
   
-  let ticker = 'last';
-  
   // Register ports and event handlers
-  component.inPorts.add('ticker', { datatype: 'string', values: ['last', 'high', 'low'] }, function (event, payload) {
-	  switch (event) {
-		  case 'data': 
-			ticker = payload;
-			return ;
-	  }
-  });
-  component.inPorts.add('in', { datatype: 'bang' }, function(event, payload) {
-    switch (event) {
-      case 'data':
-        // Forward data when we receive it.
-        // Note: send() will connect automatically if needed
-        component.outPorts.promise.send(bitcoin.getPrice(ticker)
-        	.then((lastPrice) => {
-        	component.outPorts.out.send(lastPrice);
-			return component.outPorts.out.disconnect();
-        }));
-      case 'disconnect':
-        // Disconnect output port when input port disconnects
-        component.outPorts.out.disconnect();
-		return component.outPorts.promise.disconnect();
-    }
-  });
+  component.inPorts.add('ticker', { required: true, datatype: 'string', values: ['last', 'high', 'low'] });
+  
+  component.inPorts.add('in', { datatype: 'bang' });
+  
   component.outPorts.add('out', { datatype: 'all' });
-  component.outPorts.add('promise', { datatype: 'all' });
-
+  
+  noflo.helpers.WirePattern(
+	component,
+	{
+		in: ["ticker", "in"],
+		out: "out"
+	},
+	function(data, groups, outPort) {
+		bitcoin.getPrice(data.ticker)
+		.then((lastPrice) => {
+			component.outPorts.out.send(lastPrice);
+			return component.outPorts.out.disconnect();
+			
+		})
+	}
+  );
+  
   return component; // Return new instance
 };
